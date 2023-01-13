cea90c3b2f2d5f6b9e8146e301cb14ec0366ae79
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-13 17:06:15 -0300

    Update engine.js

--- codes/engine.js
+++ codes/engine.js
@@ -1,9 +1,10 @@
 function setup (){
-  
+  createCanvas(screenX, screenY)
 }
 function draw () {
   
 }
+var screenX = window.screen.width, screenY = window.screen.height
 
 var c = 0
 //c indica qual o cenario atual, 0 sig tela de carregamento, 1 menu principal ...
