22622330504bbb00d8028824ac1999de375f7255
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-13 17:03:53 -0300

    Update engine.js

--- codes/engine.js
+++ codes/engine.js
@@ -1,1 +1,16 @@
+function setup (){
+  
+}
+function draw () {
+  
+}
 
+var c = 0
+//c indica qual o cenario atual, 0 sig tela de carregamento, 1 menu principal ...
+
+funtion preload (){
+  
+}
+function mouseClicked () {
+  
+}
