a0369c1d31bb5d7e18e8f4e0bb03429051e9177f
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-13 17:36:41 -0300

    Update engine.js

--- codes/engine.js
+++ codes/engine.js
@@ -4,17 +4,16 @@ function setup (){
 }
 function draw () {
   screenConfig();
-  frames();
   cenarios();
+  frames();
+  
 }
 
 var screenX = window.screen.width, screenY = window.screen.height
 //screenX = largura da janela do usuario, screenY = altura da janela do usuário
 var c = 0
 //c indica qual o cenario atual, 0 sig tela de carregamento, 1 menu principal ...
-var t
 function frames (){
-  t = 1
   fill (0);
   text (frameCount, 10, 10);
   text (mouseY, 40, 30);
@@ -37,7 +36,5 @@ function preload (){
 }
 // mouseClicked = função do p5 que é ativada toda vez que há um click, vc deverá usar as coordenadas do mouse para saber onde houve o click
 function mouseClicked () {
-  if (t == 1){
-    alert("funcionando")
-  }
+
 }
