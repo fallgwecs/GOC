de4474d74010a00c8945dd04ef1b9ac416327d58
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-13 17:07:19 -0300

    Update engine.js

--- codes/engine.js
+++ codes/engine.js
@@ -5,7 +5,7 @@ function draw () {
   
 }
 var screenX = window.screen.width, screenY = window.screen.height
-
+//screenX = largura da tela do usuario, screenY = altura da tela do usuário
 var c = 0
 //c indica qual o cenario atual, 0 sig tela de carregamento, 1 menu principal ...
 
