8a42eb05a2c1efde73b149cc0b78ce55fb0f2968
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-13 17:35:33 -0300

    Update engine.js

--- codes/engine.js
+++ codes/engine.js
@@ -12,12 +12,13 @@ var screenX = window.screen.width, screenY = window.screen.height
 //screenX = largura da janela do usuario, screenY = altura da janela do usuário
 var c = 0
 //c indica qual o cenario atual, 0 sig tela de carregamento, 1 menu principal ...
-
+var t
 function frames (){
+  t = 1
   fill (0);
-    text (frameCount, 10, 10);
-    text (mouseY, 40, 30);
-    text (mouseX, 40, 10);
+  text (frameCount, 10, 10);
+  text (mouseY, 40, 30);
+  text (mouseX, 40, 10);
 }
 // frameCount = variavel do p5 que indica quantos frames já foram renderizados, mouseX e mouseY = variável do p5 que indica as cordenadas do mouse
 function screenConfig (){
@@ -36,5 +37,7 @@ function preload (){
 }
 // mouseClicked = função do p5 que é ativada toda vez que há um click, vc deverá usar as coordenadas do mouse para saber onde houve o click
 function mouseClicked () {
-  
+  if (t == 1){
+    alert("funcionando")
+  }
 }
