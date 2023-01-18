9b625f157b3598c68f53d0d74e4d4f9637c91b92
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-18 16:38:01 -0300

    teste

--- codes/cenas.js
+++ codes/cenas.js
@@ -1,25 +1,33 @@
+//alterei a ordem dos ifs de propor e c, pois assim reduz o numero de ifs utilizados, otimizando ainda mais o codigo.
 function cenarios (){
-  if (c == 0){
-    if (propor == 20.9){
-      background(ld.ld20);
+  if (propor == 20.9){
+    if (c == 0){
+      //ld =loading, ou nesse caso tela de loading
+      background(ld.ld20)
+      tld20();
     }
-    if (propor == 16.9){
-      background(ld.ld16);
-    }
-    tld();
   }
-  if (c == 1){
-    background(mp.bg);
-  }  
+  if (propor == 16.9){
+    if (c == 0){
+      background(ld.ld16);
+      tld16();
+    }
+  }
 }
 var mp
-
-function tld (){
+//mp = menu principal; tld = tela de loading; bg = background
+//prefiri fazer as telas de loading separadas de acordo com a proporção assim evitando verificações desnecessarias.
+function tld20 (){
   if (propor == 20.9){
     mp = {
         bg: loadImage('assets/mP/mP-20_9.png')
     }
   }
+  if (frameCount == 100){
+    c = 1;
+  }
+}
+function tld16 (){
   if (propor == 16.9){
     mp = {
         bg: loadImage('assets/mP/mP-16_9.png')
