4d0ae36df6778ad1c05c575389f1aa5fd09faced
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-19 13:07:37 -0300

    correção de bug do code v 0.0.1.6

--- codes/cenas.js
+++ codes/cenas.js
@@ -4,48 +4,44 @@ function cenarios (){
     if (c == 0){
       //ld =loading, ou nesse caso tela de loading
       background(ld.ld20)
-      tld20();
+      tld20_1();
     }
     else if (c == 1){
       background(mp.bg);
-      Image(mp.lb, screenX*0.01, screenY*0.35, screenX*0.07, screenY*0.07);
-      Image(mp.ngb, screenX*0.01, screenY*0.40, screenX*0.07, screenY*0.07);
+      image(mp.lb, screenX*0.01, screenY*0.35, screenX*0.07, screenY*0.07);
+      image(mp.ngb, screenX*0.01, screenY*0.40, screenX*0.07, screenY*0.07);
     }
   }
   if (propor == 16.9){
     if (c == 0){
       background(ld.ld16);
-      tld16();
+      tld16_1();
     }
     else if (c == 1){
       background(mp.bg);
-      Image(mp.lb, screenX*0.01, screenY*0.35, screenX*0.07, screenY*0.07);
-      Image(mp.ngb, screenX*0.01, screenY*0.40, screenX*0.07, screenY*0.07);
+      image(mp.lb, screenX*0.01, screenY*0.35, screenX*0.07, screenY*0.07);
+      image(mp.ngb, screenX*0.01, screenY*0.40, screenX*0.07, screenY*0.07);
     }
   }
 }
 var mp
 //mp = menu principal; tld = tela de loading; bg = background; lb = loadButton ngb = newGmeButton
 //prefiri fazer as telas de loading separadas de acordo com a proporção assim evitando verificações desnecessarias.
-function tld20 (){
-  if (propor == 20.9){
-    mp = {
-        bg: loadImage('assets/mP/mP-20_9.png'),
-        lb: loadImage('assets/mP/loadButton.png'),
-        ngb: loadImage('assets/mP/newGameButton.png')
-    }
+function tld20_1 (){
+  mp = {
+    bg: loadImage('assets/mP/mP-20_9.png'),
+    lb: loadImage('assets/mP/loadButton.png'),
+    ngb: loadImage('assets/mP/newGameButton.png')
   }
   if (frameCount == 100){
     c = 1;
   }
 }
-function tld16 (){
-  if (propor == 16.9){
-    mp = {
-        bg: loadImage('assets/mP/mP-16_9.png'),
-        lb: loadImage('assets/mP/loadButton.png'),
-        ngb: loadImage('assets/mP/newGameButton.png')
-    }
+function tld16_1 (){
+  mp = {
+    bg: loadImage('assets/mP/mP-16_9.png'),
+    lb: loadImage('assets/mP/loadButton.png'),
+    ngb: loadImage('assets/mP/newGameButton.png')
   } 
   if (frameCount == 100){
     c = 1;
